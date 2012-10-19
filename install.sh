44db47c6dcee3d5c927f5dc9ded5f90683ec8f5c
Chanwit Kaewkasi <chanwit@gmail.com>
2012-10-20 01:03:33 +0700

    update installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 mkdir ~/.gent
 cd ~/.gent
-wget http://github.com/downloads/chanwit/gent/groovy-all-ivy.pack.gz
+curl -O http://cloud.github.com/downloads/chanwit/gent/groovy-all-ivy.pack.gz
 unpack200 groovy-all-ivy.pack.gz groovy-all-ivy.jar
