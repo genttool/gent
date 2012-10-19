c3a0296adc972652011366ae67e862be1d2f67b0
Chanwit Kaewkasi <chanwit@gmail.com>
2012-10-20 01:06:01 +0700

    remove pack.gz file after unpacking

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 mkdir ~/.gent
 cd ~/.gent
 curl -O http://cloud.github.com/downloads/chanwit/gent/groovy-all-ivy.pack.gz
-unpack200 groovy-all-ivy.pack.gz groovy-all-ivy.jar
+unpack200 -r groovy-all-ivy.pack.gz groovy-all-ivy.jar
